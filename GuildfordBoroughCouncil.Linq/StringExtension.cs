c0cc249a52230bf01eb04f5770feebc418e51cb9
Ben <BforBen@users.noreply.github.com>
2015-07-23 16:52:56 +0100

    Add new string extensions

--- GuildfordBoroughCouncil.Linq/StringExtension.cs
+++ GuildfordBoroughCouncil.Linq/StringExtension.cs
@@ -22,6 +22,25 @@ namespace GuildfordBoroughCouncil.Linq
         {
             return str.Replace("___", " - ").Replace("__", "-").Replace("_", " ");
         }
+        
+        public static string GenerateSlug(this string phrase)
+        {
+            string str = phrase.RemoveAccent().ToLower();
+            // invalid chars           
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+            // convert multiple spaces into one space   
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+            // cut and trim 
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            return str;
+        }
+
+        public static string RemoveAccent(this string txt)
+        {
+            byte[] bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(txt);
+            return System.Text.Encoding.ASCII.GetString(bytes);
+        }
 
         /// <summary>
         /// Returns a value indicating whether a value in the specified System.String[] object occurs within this string.
